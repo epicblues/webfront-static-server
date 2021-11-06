7d665300ffca89b09f5ff72f6be31bbac0c2f291
KMS <epicblue@hanmail.net>
2021-11-06 21:05:59 +0900

    dist folder gitignore

--- dist/controllers/image.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.main = void 0;
-var main = function (req, res) {
-    res.status(200).json({ message: "This is file Router main page" });
-};
-exports.main = main;
-//# sourceMappingURL=image.js.map
