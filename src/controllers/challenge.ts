a9706fb3edc05cddd18f2c3bdd65bf09466562f7
KMS <epicblue@hanmail.net>
2021-11-29 21:37:28 +0900

    챌린지 이미지 전송 폼 테스트

--- src/controllers/challenge.ts
+++ src/controllers/challenge.ts
@@ -20,44 +20,51 @@ export const createChallenge: RequestHandler = async (req, res) => {
     if (error) throw error;
     logger.info(fields);
     logger.info(files);
-    // const challengeId = await getNextSequence("challenge", client);
-    // const challengeForm = req.body;
-    // // challenge_[chal_id].jpg;
-    // const imageName = `challenge_${challengeId}.jpg`;
+    const challengeId = await getNextSequence("challenge", client);
+    const challengeForm: any = {};
+    Object.keys(fields).forEach((key) => {
+      challengeForm[key] = ["dateDiff", "userId"].includes(key)
+        ? +fields[key][0]
+        : fields[key][0];
+    });
+    // challenge_[chal_id].jpg;
+    const imageName = `challenge_${challengeId}.jpg`;
 
-    // await resizeAndDeleteOriginalImg(
-    //   files.image[0].path,
-    //   `/public/static/${imageName}`
-    // );
-    // // Diet Type
-    // if (challengeForm.type === "diet") {
-    //   const dietChecker = [];
-    //   for (let i = 0; i < challengeForm.dateDiff + 1; i++)
-    //     dietChecker.push(false);
-    //   challengeForm.diet.checker = dietChecker;
-    // }
+    await resizeAndDeleteOriginalImg(
+      files.image[0].path,
+      `/public/static/${imageName}`
+    );
+    // Diet Type
+    if (challengeForm.type === "diet") {
+      const dietChecker = [];
+      for (let i = 0; i < challengeForm.dateDiff + 1; i++)
+        dietChecker.push(false);
+      challengeForm.diet = JSON.parse(challengeForm.diet);
+      challengeForm.diet.checker = dietChecker;
+    }
 
-    // // Recipe Type
-    // if (challengeForm.type === "recipe") {
-    //   challengeForm.recipe.checker = [];
-    // }
+    // Recipe Type
+    if (challengeForm.type === "recipe") {
+      challengeForm.recipe = JSON.parse(challengeForm.recipe);
+      challengeForm.recipe.checker = [];
+    }
 
-    // await client
-    //   .db("webfront")
-    //   .collection("challenge")
-    //   .insertOne({
-    //     ...challengeForm,
-    //     _id: challengeId,
-    //     uploadDate: new Date(),
-    //     startDate: new Date(challengeForm.startDate),
-    //     endDate: new Date(challengeForm.endDate),
+    await client
+      .db("webfront")
+      .collection("challenge")
+      .insertOne({
+        ...challengeForm,
+        _id: challengeId,
+        uploadDate: new Date(),
+        startDate: new Date(challengeForm.startDate),
+        endDate: new Date(challengeForm.endDate),
+        userId,
+        participants: [challengeForm.userId],
+        winners: [],
+        image: `/static/${imageName}`,
+      });
 
-    //     participants: [challengeForm.userId],
-    //     winners: [],
-    //     image: `/static/${imageName}`,
-    //   });
-
-    res.status(200).json({ fields, files });
+    res.status(200).json({ challengeForm, files });
   } catch (error) {
     res.status(404).json({ message: error });
   }
