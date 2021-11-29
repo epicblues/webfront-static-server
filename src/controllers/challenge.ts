fd8fdcc0568770a8de2bbc115f27de48d405bb43
KMS <epicblue@hanmail.net>
2021-11-29 21:02:36 +0900

    챌린지 formData 테스트

--- src/controllers/challenge.ts
+++ src/controllers/challenge.ts
@@ -5,13 +5,60 @@ import { ImageFile, Ingredient } from "../models";
 import fs from "fs";
 import path from "path";
 import multiparty from "multiparty";
+import {
+  getParsedFormData,
+  resizeAndDeleteOriginalImg,
+} from "../util/multipart";
+import { logger } from "../util/logger";
 
 export const createChallenge: RequestHandler = async (req, res) => {
-  // 폼 데이터로 오지 않을 가능성 존재
-  const { id: user_id } = JSON.parse(req.headers.authorization as string);
-  const multiHandler = new multiparty.Form({
-    uploadDir: path.join("./", "public/static"),
-  });
+  const { id: userId } = JSON.parse(req.headers.authorization as string);
 
-  res.status(200).json({ message: "createChallenge" });
+  try {
+    const client = await clientPromise;
+    const [error, fields, files] = await getParsedFormData(req);
+    if (error) throw error;
+    logger.info(fields);
+    logger.info(files);
+    // const challengeId = await getNextSequence("challenge", client);
+    // const challengeForm = req.body;
+    // // challenge_[chal_id].jpg;
+    // const imageName = `challenge_${challengeId}.jpg`;
+
+    // await resizeAndDeleteOriginalImg(
+    //   files.image[0].path,
+    //   `/public/static/${imageName}`
+    // );
+    // // Diet Type
+    // if (challengeForm.type === "diet") {
+    //   const dietChecker = [];
+    //   for (let i = 0; i < challengeForm.dateDiff + 1; i++)
+    //     dietChecker.push(false);
+    //   challengeForm.diet.checker = dietChecker;
+    // }
+
+    // // Recipe Type
+    // if (challengeForm.type === "recipe") {
+    //   challengeForm.recipe.checker = [];
+    // }
+
+    // await client
+    //   .db("webfront")
+    //   .collection("challenge")
+    //   .insertOne({
+    //     ...challengeForm,
+    //     _id: challengeId,
+    //     uploadDate: new Date(),
+    //     startDate: new Date(challengeForm.startDate),
+    //     endDate: new Date(challengeForm.endDate),
+
+    //     participants: [challengeForm.userId],
+    //     winners: [],
+    //     image: `/static/${imageName}`,
+    //   });
+
+    res.status(200).json({ fields, files });
+  } catch (error) {
+    res.status(404).json({ message: error });
+  }
 };
